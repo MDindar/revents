a3d24f8556e3551d1395df332c4cefea4fb11146
Mostafa Dindar <dindar.com@gmail.com>
2021-02-19 22:10:18 +0330

    end of section 3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/events/eventDashbord/EventDashboard";
+import NavBar from "../../features/nav/NavBar";
 
 function App() {
+  const [formOpen, setFormOpen] = useState(false);
   return (
-    <div className="App">
-      <h1>Re-vents</h1>
-    </div>
+    <Fragment>
+      <NavBar setFormOpen={setFormOpen} />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <Container>
+        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} />
+      </Container>
+    </Fragment>
   );
 }
 
